3abe5dbe7f4a28362ada9cd7ef4eb5f3c9947796
ikhsanfrcn <ikhsanfrcn@gmail.com>
2025-03-11 15:32:24 +0700

    update officedata

--- src/data/officeProjectsData.ts
+++ src/data/officeProjectsData.ts
@@ -3,21 +3,21 @@ export const officeProjectsData = [
         caption: "HTML CSS TailwindCSS Laravel PHP",
         title: "BVR Group Asia",
         description: "Website for BVR Group Asia",
-        buttons: [{ title: "View <~>", onClick: () => { window.open("https://bvrgroupasia.com", "_blank"); }, className: "border-primary" }],
+        buttons: [{ title: "Visit <~>", onClick: () => { window.open("https://bvrgroupasia.com", "_blank"); }, className: "border-primary" }],
     },
     {
         caption: "HTML CSS TailwindCSS Laravel Livewire PHP",
         title: "BVR Property",
         description: "Website for BVR Group Asia",
-        buttons: [{ title: "View <~>", onClick: () => { window.open("https://bvrbaliholidayrentals.com", "_blank"); }, className: "border-primary" }],    },
+        buttons: [{ title: "Visit <~>", onClick: () => { window.open("https://bvrbaliholidayrentals.com", "_blank"); }, className: "border-primary" }],    },
     {
         caption: "HTML CSS TailwindCSS JQuery Laravel",
         title: "Sirfin Global",
         description: "Website Sirfin Global",
-        buttons: [{ title: "View <~>", onClick: () => { window.open("https://sirfinglobal.com", "_blank"); }, className: "border-primary" }],    },
+        buttons: [{ title: "Visit <~>", onClick: () => { window.open("https://sirfinglobal.com", "_blank"); }, className: "border-primary" }],    },
     {
         caption: "HTML CSS TailwindCSS Laravel Livewire PHP",
         title: "Bali Coffe Club",
         description: "Website for Bali Coffe Club",
-        buttons: [{ title: "View <~>", onClick: () => { window.open("https://balicoffeeclub.com", "_blank"); }, className: "border-primary" }],    },
+        buttons: [{ title: "Visit <~>", onClick: () => { window.open("https://balicoffeeclub.com", "_blank"); }, className: "border-primary" }],    },
 ];
