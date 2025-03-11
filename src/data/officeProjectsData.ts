83ab5af88f2ba2525ac7be01b7697f21d402b80a
ikhsanfrcn <ikhsanfrcn@gmail.com>
2025-03-11 15:31:49 +0700

    update officedata

--- src/data/officeProjectsData.ts
+++ src/data/officeProjectsData.ts
@@ -3,24 +3,21 @@ export const officeProjectsData = [
         caption: "HTML CSS TailwindCSS Laravel PHP",
         title: "BVR Group Asia",
         description: "Website for BVR Group Asia",
-        buttons: [{ title: "Cached >=", onClick: () => { }, className: "border-gray text-gray" }],
+        buttons: [{ title: "View <~>", onClick: () => { window.open("https://bvrgroupasia.com", "_blank"); }, className: "border-primary" }],
     },
     {
         caption: "HTML CSS TailwindCSS Laravel Livewire PHP",
         title: "BVR Property",
         description: "Website for BVR Group Asia",
-        buttons: [{ title: "Cached >=", onClick: () => { }, className: "border-gray text-gray" }],
-    },
+        buttons: [{ title: "View <~>", onClick: () => { window.open("https://bvrbaliholidayrentals.com", "_blank"); }, className: "border-primary" }],    },
     {
         caption: "HTML CSS TailwindCSS JQuery Laravel",
         title: "Sirfin Global",
         description: "Website Sirfin Global",
-        buttons: [{ title: "Cached >=", onClick: () => { }, className: "border-gray text-gray" }],
-    },
+        buttons: [{ title: "View <~>", onClick: () => { window.open("https://sirfinglobal.com", "_blank"); }, className: "border-primary" }],    },
     {
         caption: "HTML CSS TailwindCSS Laravel Livewire PHP",
         title: "Bali Coffe Club",
         description: "Website for Bali Coffe Club",
-        buttons: [{ title: "Cached >=", onClick: () => { }, className: "border-gray text-gray" }],
-    },
+        buttons: [{ title: "View <~>", onClick: () => { window.open("https://balicoffeeclub.com", "_blank"); }, className: "border-primary" }],    },
 ];
