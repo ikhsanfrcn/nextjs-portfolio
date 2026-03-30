889e78039f3f4ee03ec298d3449d52d55ef4418e
ikhsanfrcn <ikhsanfrcn@gmail.com>
2026-03-30 16:36:47 +0700

    update data

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -1,5 +1,12 @@
 export const experienceData = [
   {
+    startDate: "Jan 2024",
+    endDate: "Present",
+    companyName: "Freelance",
+    description:
+      "I develop and maintain web applications using PHP Native, Laravel, and WordPress, focusing on security, performance, and user experience. I optimize HTML, Tailwind CSS, and React.js code, troubleshoot technical issues, assist with system setups, and provide ongoing support. I collaborate with UI/UX designers and deliver technical solutions to meet client needs.",
+  },
+  {
     startDate: "Aug 2022",
     endDate: "Dec 2023",
     companyName: "BVR Group Asia",
@@ -19,5 +26,19 @@ export const experienceData = [
     companyName: "MyRepvblic FiveM Game Server",
     description:
       "As the owner of a custom FiveM game server, I manage server creation, optimization, and maintenance. While using open-source scripts, I ensure compatibility and troubleshoot common issues. I use Lua for client-side and server-side scripting to personalize the user experience, overseeing daily operations, performance, user management, and database updates for smooth gameplay.",
+  },
+  {
+    startDate: "Jan 2017",
+    endDate: "Dec 2018",
+    companyName: "Grandia Hotel",
+    description:
+      "Maintained hotel IT systems, including PMS, POS, and CCTV. Troubleshot hardware, software, and network issues (LAN/WLAN, VoIP, Guest WiFi). Ensured server uptime for guest and back-office operations, handled system setups, and performed hardware repairs. Supported printers, billing systems, and daily IT operations while collaborating with the IT team.",
+  },
+  {
+    startDate: "July 2016",
+    endDate: "Dec 2016",
+    companyName: "CV Global Media Solusindo",
+    description:
+      "Provided on-site IT support, including installation and configuration of Windows OS, software, and office apps. Troubleshot hardware/software issues (PC, laptop, printer) and assisted with network setup (LAN/WiFi). Supported IT infrastructure deployment, maintenance, and managed asset inventory and documentation.",
   },
 ];
